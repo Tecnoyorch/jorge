11090cc74b1193ffd232487fc53108b2f9e65cdb
tecnoyorch <andres.flores@udgvirtual.udg.mx>
2023-10-30 19:35:58 -0600

    Tecnoyorch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
-import logo from '../logo.svg';
 import '../App.css';
 
 function App() {
   return (
     <div className="container">
-    <h1 className="title">Hola Mundo probando<span aria-label="emoji"role="img">🔥</span></h1>
-    <input type="text" className="new-task"/>
-    <h2>Si se puede</h2>
+    <h1 className="title">Tecnoyorch<span aria-label="emoji"role="img">📱</span></h1>
+    
+    <h2>El Hospital para tu Cel</h2>
+    <div class="Display">
+      <ul>
+        <li>Cambio de display</li>
+        <li>reparación y/o cambio de cristal</li>
+        <li>Venta de micas de cristal</li>
+      </ul>
+
+    </div>
+    <table>
+      <td class="tel">Servicio para Telefonos que no prenden</td>
+      <tr>Diagnotico de bateria</tr>
+      <tr>Diagnostico boton power</tr>
+      <tr>Diagnostico tarjeta madre</tr>
+    </table>
+
+    
+
+
    </div>
    
   );
